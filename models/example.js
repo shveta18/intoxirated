285eaebe431698b1e6a43658a8e1dfa028dd077e
Billy Behman <billybehman@gmail.com>
2020-05-25 13:54:27 -0400

    added a stand in database and table and some html

--- models/example.js
+++ models/example.js
@@ -1,7 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Wines = sequelize.define("Wines", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+        }
+    },
+    maker: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        max: 5,
+        min: 1
+      }
+    }
   });
-  return Example;
+  return Wines;
 };
