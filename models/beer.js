4fea64ad9378d79e5f0cd2cb8ce6cf604011d706
Billy Behman <billybehman@gmail.com>
2020-05-30 11:10:50 -0400

    adding models and wineFrontEnd

---
+++ models/beer.js
@@ -0,0 +1,45 @@
+module.exports = function (sequelize, DataTypes) {
+    var Beer = sequelize.define("Beer", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        brewer: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        style: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        alchoholByVolume: {
+            type: DataTypes.DECIMAL,
+            allowNull: true,
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                max: 10,
+                min: 1
+            }
+        }
+    });
+    return Beer;
+};
---
+++ models/whiskey.js
@@ -0,0 +1,41 @@
+module.exports = function (sequelize, DataTypes) {
+    var Whiskey = sequelize.define("Whiskey", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        manufacturer: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        style: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                len: [4, 4]
+            }
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                max: 10,
+                min: 1
+            }
+        }
+    });
+    return Whiskey;
+};
