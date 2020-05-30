44ab7ba20b79f2073641ca072e7a384a182c64e4
Billy Behman <billybehman@gmail.com>
2020-05-30 13:04:28 -0400

    why

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,8 +15,8 @@ module.exports = function(app) {
   
   app.get("/wine", function(req, res) {
     db.Wine.findAll({}).then(function(data) {
-      console.log(data.dataValues)
-      res.render("wine", {wines: data.dataValues})
+      console.log(data)
+      res.render("wine", {wines: data})
     });
   });
 
