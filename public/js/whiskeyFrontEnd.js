81adf7e36e5076f8d7791f14f21d983578f6711c
Shveta Kumar <sk@shveta.ca>
2020-06-03 20:11:45 -0400

    Ammended files to be able to send GET request from front end to api and query db. Query returns search results

--- public/js/whiskeyFrontEnd.js
+++ public/js/whiskeyFrontEnd.js
@@ -1,20 +1,48 @@
+// User clicks the submit button to check current results. 
 $("#submit").on("click", function(event){
-    event.preventDefault()
+    event.preventDefault();
     var newWhiskey = {
-        name: $("#drinkCategorySelect option:selected").val(),
-        manufacturer: $("#manufacturer").val(),
+        drinktype: $("#drinkCategorySelect option:selected").val(),
+        name: $("#name").val(),
+        manufacturer: $("#manf-brew-dist").val(),
         style: $("#style").val(),
         rating: $("#rating").val()
     }
 console.log(newWhiskey);
-    $.ajax({
-        type: "POST",
-        url: "/api/whiskey",
-        data: newWhiskey
-    }).then(function(res) {
-        console.log(res)
-    })
+//Create a GET requeset to api-routes.js and get results from Database.
+$.ajax({
+    method: "GET",
+    dataType: "JSON",
+    url: "/api/search",
+    data: newWhiskey
+}).then(function(res) {
+    console.log(res);
+    // we will get a result back. I want to show into a nice table on the same page, i.e empty container. 
+    // first create a table within the index.handlebars
+    /*
+    res = [
+        {
+            name: "Asas",
+            age: "asda"
+        },
+        ...
+    ]
+    */
+});
 
-})
+});
+//using the above search criteria, query the SQL dB. 
+
+
+
+
+    // $.ajax({
+    //     type: "POST",
+    //     url: "/api/whiskey",
+    //     data: newWhiskey
+    // }).then(function(res) {
+    //     console.log(res)
+    // });
+
 
 
