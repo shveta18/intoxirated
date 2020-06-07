d6c8a793900860f7881f2966765a945f1483af36
Shveta Kumar <sk@shveta.ca>
2020-06-07 14:51:04 -0400

    New beverage ratings are being added to db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -37,6 +37,7 @@ module.exports = function (app) {
         }
       }).then(function (data) {
         console.log(data);
+
         res.json(data);
       });
     } else {
@@ -44,85 +45,113 @@ module.exports = function (app) {
     }
   });
 
-
-  // USER LOGIN CHECK in SQL DB 
-
-  app.post("/user-login", function (req, res) {
-    console.log("Login details sent:");
+  // USER SUBMITS NEW RECORD / RATING
+  app.post("/api/rate", function (req, res) {
+    console.log("New Ratings sent:");
     console.log(req.body);
-    // code to verify if username and password combination exists in db 
-    db.Users.findOne({
-      where: {
-        userid: req.body.userid, password: req.body.password
-      }
-    }).then(function (data) {
-      console.log(data);
-      console.log(data.length);
-      //store the user id as a req session id. 
 
-      if (data != null && data != '' && data != []) {
-        // res.redirect('/myratings');
-        req.session.user = data.dataValues;
-        console.log("user session id: ");
-        console.log(req.session.user);
-        res.json(
-          {
-            "result": "success"
+    if (req.session.user && req.cookies.sid) {
+      // get the user session and user id from the request
+      var loggedUserId = req.session.user.id;
+      var category = req.body.category;
+      var name = req.body.name;
+      var manufacturer = req.body.manufacturer;
+      var style = req.body.style;
+      var rating = req.body.rating;
+      // code to verify if username and password combination exists in db 
+      db.UserRatings.create({
+        category: category,
+        name: name,
+        manufacturer: manufacturer,
+        style: style,
+        rating: rating,
+        UserId: loggedUserId,
+      }
+      ).then(function (data) {
+        console.log(data);
+      });
+    }
+
+  });
+
+      // USER LOGIN CHECK in SQL DB 
+
+      app.post("/user-login", function (req, res) {
+        console.log("Login details sent:");
+        console.log(req.body);
+        // code to verify if username and password combination exists in db 
+        db.Users.findOne({
+          where: {
+            userid: req.body.userid, password: req.body.password
           }
-        );
+        }).then(function (data) {
+          console.log(data);
+          console.log(data.length);
+          //store the user id as a req session id. 
 
-      } else {
-        res.json({ "result": "failure" });
-      }
-    });
-  });
-  // Create a new user in SQL DB when a new user registers
-  app.post("/user-registration", function (req, res) {
-    console.log("User data being sent to SQL");
-    console.log(req.body);
-    db.Users.create({ userid: req.body.userid, firstName: req.body.firstName, lastName: req.body.lastName, password: req.body.password }).then(function (data) {
+          if (data != null && data != '' && data != []) {
+            // res.redirect('/myratings');
+            req.session.user = data.dataValues;
+            console.log("user session id: ");
+            console.log(req.session.user);
+            res.json(
+              {
+                "result": "success"
+              }
+            );
 
-      res.json(data)
-    });
-  });
+          } else {
+            res.json({ "result": "failure" });
+          }
+        });
+      });
+      // Create a new user in SQL DB when a new user registers
+      app.post("/user-registration", function (req, res) {
+        console.log("User data being sent to SQL");
+        console.log(req.body);
+        db.Users.create({ userid: req.body.userid, firstName: req.body.firstName, lastName: req.body.lastName, password: req.body.password }).then(function (data) {
 
-  // When user submits a WINE, it gets added to the userRatings table
-  app.post("/api/wine", function (req, res) {
-    db.UserRatings.create(req.body).then(function (data) {
-      res.json(data)
-    });
-  });
+          res.json(data)
+        });
+      });
 
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-  // Get all results from the sql db for the search criteria. 
-  // app.get("/api/whiskey", function(req, res) {
-  // db.
+      // When user submits a WINE, it gets added to the userRatings table
+      app.post("/api/wine", function (req, res) {
+        db.UserRatings.create(req.body).then(function (data) {
+          res.json(data)
+        });
+      });
 
-  // })
+      app.get("/api/examples", function (req, res) {
+        db.Example.findAll({}).then(function (dbExamples) {
+          res.json(dbExamples);
+        });
+      });
+      // Get all results from the sql db for the search criteria. 
+      // app.get("/api/whiskey", function(req, res) {
+      // db.
 
-  // Create a new example
-  app.post("/api/wine", function (req, res) {
-    db.Wine.create(req.body).then(function (data) {
-      res.json(data)
-    })
-  })
+      // })
 
-  app.post("/api/beer", function (req, res) {
-    db.Wine.create(req.body).then(function (data) {
-      res.json(data)
-    })
-  })
+      // Create a new example
+      app.post("/api/wine", function (req, res) {
+        db.Wine.create(req.body).then(function (data) {
+          res.json(data)
+        })
+      })
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
-};
+      app.post("/api/beer", function (req, res) {
+        db.Wine.create(req.body).then(function (data) {
+          res.json(data)
+        })
+      })
+
+      // Delete an example by id
+      app.delete("/api/examples/:id", function (req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function (
+          dbExample
+        ) {
+          res.json(dbExample);
+        });
+      });
+    };
